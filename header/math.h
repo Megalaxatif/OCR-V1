061e1f42f28ae4733ac63c6d8cb125e4e84a0a33
Megalaxatif <megalaxatif@alphaMeg.home>
2026-07-03 18:34:35 +0200

    matrix arithmetic

---
+++ header/math.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Mat{
+  double** data;
+  size_t col;
+  size_t row;
+};
+
+struct Mat* MatCreate(size_t row, size_t col, double* data[]);
+struct Mat* MatTranspose(struct Mat* mat);
+void MatDestroy(struct Mat* mat);
+void MatPrint(struct Mat* mat);
+struct Mat* MatAdd(struct Mat* mat_1, struct Mat* mat_2);
+struct Mat* MatHadamard(struct Mat* mat_1, struct Mat* mat_2);
+struct Mat* MatMult(struct Mat* mat_1, struct Mat* mat_2);
---
+++ math.c
@@ -0,0 +1,103 @@
+#include "header/math.h"
+
+
+struct Mat* MatCreate(size_t row, size_t col, double* data[]){
+    if (row <= 0 || col <= 0) {
+        printf("Error: MatCreate, invalid row or col value\n");
+        return NULL;
+    }
+
+    struct Mat* newMat = malloc(sizeof(struct Mat));
+
+    newMat->data = calloc(sizeof(double*), row);
+    newMat->row = row;
+    newMat->col = col;
+
+    for(int i = 0; i < row; i++){
+        newMat->data[i] = calloc(sizeof(double), col);
+    }
+
+    if (data != NULL){
+        for(size_t i = 0; i < newMat->row; i++){
+            for(size_t j = 0; j < newMat->col; j++){
+                newMat->data[i][j] = data[i][j];
+            }
+        }
+    }
+    return newMat;
+}
+
+struct Mat* MatTranspose(struct Mat* mat){
+    struct Mat* transpose = MatCreate(mat->col, mat->row, NULL);
+    for(size_t i = 0; i < mat->row; i++){
+        for(size_t j = 0; j < mat->col; j++){
+            transpose->data[j][i] = mat->data[i][j];
+        }
+    }
+    return transpose;
+}
+
+void MatDestroy(struct Mat* mat){
+    for(size_t i = 0; i < mat->row; i++){
+        free(mat->data[i]);
+    }
+    free(mat->data);
+    free(mat);
+}
+
+
+void MatPrint(struct Mat* mat){
+    for(size_t i = 0; i < mat->row; i++){
+        for(size_t j = 0; j < mat->col; j++){
+            printf("%f ", mat->data[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+
+struct Mat* MatAdd(struct Mat* mat_1, struct Mat* mat_2){ // add mat_2 to mat_1 and return mat_1
+    if (mat_1->col != mat_2->col || mat_1->row != mat_2->row){
+        printf("Error: MatAdd, the matrices don't have the same size\n");
+        return NULL;
+    }
+    for(size_t i = 0; i < mat_1->row; i++){
+        for(size_t j = 0; j < mat_1->col; j++){
+            mat_1->data[i][j] += mat_2->data[i][j];
+        }
+    }
+    return mat_1;
+}
+
+
+struct Mat* MatHadamard(struct Mat* mat_1, struct Mat* mat_2){ // perform the Hadamard product between mat_1 and mat_2 and return mat_1
+    if (mat_1->col != mat_2->col || mat_1->row != mat_2->row){
+        printf("Error: MatHadamard, the matrices don't have the same size\n");
+        return NULL;
+    }
+    for(size_t i = 0; i < mat_1->row; i++){
+        for(size_t j = 0; j < mat_1->col; j++){
+            mat_1->data[i][j] *= mat_2->data[i][j];
+        }
+    }
+    return mat_1;
+}
+
+
+struct Mat* MatMult(struct Mat* mat_1, struct Mat* mat_2){ // perform the matrix multiplication between mat_1 and mat_2 and return the resulting matrix
+    if (mat_1->col != mat_2->row){
+        printf("Error: MatMult, the matrices don't have compatible size\n");
+        return NULL;
+    }
+    struct Mat* res = MatCreate(mat_1->row, mat_2->col, NULL);
+    for(int j = 0; j < mat_2->col; j++){
+        for(int i = 0; i < mat_1->row; i++){
+            double tmp = 0;
+            for(int k = 0; k < mat_2->row; k++){
+                tmp += mat_1->data[i][k] * mat_2->data[k][j];
+            }
+            res->data[i][j] = tmp;
+        }
+    }
+    return res;
+}
